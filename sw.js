48df876ea259dadcdc0858a240129d9e3c2d2969
pemesananbajuonline <pemesananbajuonline@gmail.com>
2026-05-17 19:46:20 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 /* ================= SERVICE WORKER CACHE =============== */
 /* ===================================================== */
 
-const CACHE_NAME = "order-online-cache-v39";
+const CACHE_NAME = "order-online-cache-v40";
 
 const FILES_TO_CACHE = [
     "./",
